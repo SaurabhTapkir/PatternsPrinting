9d65395a4a08fa3542473a8278a7964927bd2870
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-09-29 10:25:09 -0700

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,11 @@
+class pattern1{
+  public static void main(String[] args)
+  {
+    for(int r=0;r<5;r++)
+      for (int c=0;c<5c++)
+      {
+      System.out.print("*");  
+      }
+    System.out.printlm();
+  }
+}
