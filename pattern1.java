85c99f0b3bc9490a8165423a424ec01d702cdeb3
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-09-29 10:27:07 -0700

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,8 +1,19 @@
+/*
+print pattern:
+    *****
+    *****
+    *****
+    *****
+
+*/
+
+
+
 class pattern1{
   public static void main(String[] args)
   {
     for(int r=0;r<5;r++)
-      for (int c=0;c<5c++)
+      for (int c=0;c<5;c++)
       {
       System.out.print("*");  
       }
