d4ad58c21f3c4d0293df6320f29770313c5b2ee7
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-02 10:30:56 -0700

    Create Pattern03.java
    print pattern:
    
    01010
    01010
    01010
    01010
    01010

---
+++ Pattern03.java
@@ -0,0 +1,31 @@
+/*
+print :
+01010
+01010
+01010
+01010
+01010
+
+*/
+
+class Pattern03
+  {
+    public static void main(String[] args)
+    {
+      for(int i=0;i<5;i++)
+        {
+          for(int j=0;j<5;j++)
+            {
+              if(j%2==0)
+              {
+                System.out.print("0");
+              }
+              else
+              {
+                System.out.print("1");
+              }
+            }
+          System.out.println();
+        }
+    }
+  }
