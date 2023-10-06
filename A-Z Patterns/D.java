bdd7a03b74bbf0f11e53a9e2b6b35c0b259ba384
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-06 10:36:45 -0700

    Add files via upload
    Print Letter D using *

---
+++ A-Z Patterns/D.java
@@ -0,0 +1,19 @@
+public class D {
+   public static void  main(String[] args)
+    {
+        for(int r=0;r<5;r++)
+        {
+           for(int c=0;c<5;c++)
+           {
+            if(r==0||c==1.||r==4||c==4)
+            {
+           System.out.print("*" +" ");
+            }
+            else{
+                System.out.print("  ");
+            }
+           } 
+           System.out.println();
+        }
+    }
+}
