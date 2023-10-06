633202c2dad9359d7c763f2b7cec200f2ee371be
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-06 10:38:14 -0700

    Add files via upload
    Print Letter E

---
+++ A-Z Patterns/E.java
@@ -0,0 +1,19 @@
+public class E {
+   public static void main(String[] args)
+    {
+        for(int r=0;r<5;r++)
+        {
+            for(int c=0;c<5;c++)
+            {
+                if(r==0||c==0||r==2||r==4)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print("");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
