78c86fd7185530897243a17a041dae3550fd343c
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-06 10:42:30 -0700

    Add files via upload
    Print Letter F

---
+++ A-Z Patterns/F.java
@@ -0,0 +1,20 @@
+public class F {
+    public static void main(String[] args)
+    {
+        for(int r=0;r<5;r++)
+        {
+            for(int c=0;c<5;c++)
+            {
+                if(r==0||c==0||r==2)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                   System.out.print(" ");  
+                }
+            }
+            System.out.println();
+        }
+    }
+}
