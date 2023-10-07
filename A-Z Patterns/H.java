52a9c98817966a30672509a98977f1b6af8b07b9
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-07 09:19:06 -0700

    Add files via upload
    Printing letter H

---
+++ A-Z Patterns/H.java
@@ -0,0 +1,22 @@
+public class H {
+    public static void main(String[] args)
+    {
+        for(int r=0;r<5;r++)
+        {
+            for(int c=0;c<5;c++)
+            {
+                if(c==0||c==4||r==2)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
