34040b4c08fef138816e707a107caeabe66d3bf0
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-08 10:14:02 -0700

    Add files via upload
    Print Letter J

---
+++ A-Z Patterns/J.java
@@ -0,0 +1,22 @@
+public class J {
+   public static void main(String[] args)
+   {
+    int n=5;
+    for(int r=0;r<n;r++)
+    {
+        for(int c=0;c<n;c++)
+        {
+            if((c==n-1)||(r==n-1)||(c==0 && r>=n-2))
+            {
+                System.out.print("* ");
+            }
+            else
+            {
+                System.out.print("  ");
+            }
+
+        }
+        System.out.println();
+    }
+   } 
+}
