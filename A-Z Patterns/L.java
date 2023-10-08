2b1eb35a26218270d72fab7b2531d3eb8e833e6a
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-08 10:17:38 -0700

    Add files via upload
    Print Letter L

---
+++ A-Z Patterns/L.java
@@ -0,0 +1,22 @@
+public class L {
+public static void   main(String[] args)
+    {
+        int n=5;
+        for(int r=0;r<n;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                if(c==0||r==n-1)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
