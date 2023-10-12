816887635b45b65d4da05ef424cf6c9ed178e29a
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-12 21:55:29 +0530

    Add files via upload

---
+++ A-Z Patterns/R.java
@@ -0,0 +1,21 @@
+public class R{
+    public static void main(String[] args)
+    {
+        int n=5;
+        for(int r=0;r<n;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                if(c==0||r==0||r==2||(c==4 && r==1) ||(c==r&& r>2))
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
