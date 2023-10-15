0d1c3eb267b8b7736f8dca8c788b58cfc16f98e0
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-15 23:34:13 +0530

    Add files via upload

---
+++ A-Z Patterns/X.java
@@ -0,0 +1,20 @@
+public class X {
+    public static void main(String[] args)
+    {int n=5;
+        for(int r=0;r<n;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                if(c==r||c+r==n-1)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
