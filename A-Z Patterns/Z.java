d0ad95f7215fd6c9b5e8c84fa96fae4829761439
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-15 23:35:11 +0530

    Add files via upload

---
+++ A-Z Patterns/Z.java
@@ -0,0 +1,21 @@
+public class Z {
+    
+    public static void main(String[] args)
+    {
+        for(int r=0;r<5;r++)
+        {
+            for(int c=0;c<5;c++)
+            {
+                if(r==0||r==4||c+r==4)
+                {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
