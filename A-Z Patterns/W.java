93b8e00c0dda2804283667363bd4ece0654efa43
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-10-15 23:33:26 +0530

    Add files via upload

---
+++ A-Z Patterns/W.java
@@ -0,0 +1,21 @@
+public class W {
+    public static void main(String[] args) {
+        int n=5;
+ 
+        for(int r=0;r<n;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                if(c==0||c==n-1||c+r==n-1&&r>n/2||c==r &&c>=n/2)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
